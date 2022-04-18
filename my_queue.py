c5f3bfb93cf15fe9b00badc38fcf23f522a9e9c4
MarziyehKarimi <93284130+MarziyehKarimi@users.noreply.github.com>
2022-04-18 18:16:37 +0430

    queue

---
+++ my_queue.py
@@ -0,0 +1,41 @@
+class Queue:
+    class Node:
+        def __init__(self,data):
+            self.data = data
+            self.next = None
+
+    def __init__(self):
+        self.head = None
+        self.last = None
+        self.size = 0
+
+
+    def is_empty(self): #complexity : tete(1)
+        return self.size == 0
+
+    def enqueue(self,data): #complexity : tete(1)
+        new = self.Node(data)
+        if self.is_empty():
+            self.head = new
+        else:
+            self.tail.next = new
+        self.tail = new
+        self.size += 1
+
+    def dequeue(self): #complexity : tete(1)
+        if self.is_empty():
+            raise AssertionError('empty queue can not dequeue')
+        result = self.head.data
+        self.head = self.head.next
+        self.size -= 1
+        if self.is_empty():
+            self.last = None
+        return result
+
+
+
+
+
+
+
+    
